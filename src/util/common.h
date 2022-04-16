d876488e7ff6787abe4d523353b5d58ce6c9c263
Ubuntu <benitakbritto@machine2.0ccfk3frr5qehmqm4x4bwzb4vd.gx.internal.cloudapp.net>
2022-04-16 01:11:17 +0000

    init common.h

--- src/util/common.h
+++ src/util/common.h
@@ -1,1 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
 
+/******************************************************************************
+ * MACROS
+ *****************************************************************************/
+#define DEBUG                       0                  
+#define dbgprintf(...)              if (DEBUG) { printf(__VA_ARGS__); }
+#define CRASH_TEST                  0
+#define crash()                     if (CRASH_TEST) { *((char*)0) = 0; }
+
+#endif
