346a76e3912a5109bee126ce72bf06083c38e4fe
reetuparna <reetuparna.1988@gmail.com>
2022-04-16 18:22:47 +0000

    leveldb wraooer

---
+++ src/util/levelDBWrapper.cc
@@ -0,0 +1,16 @@
+#include "levelDBWrapper.h"
+
+Status LevelDBWrapper::Get(string key, string &value) {
+    return db->Get(ReadOptions(), key, &value);
+}
+
+Status LevelDBWrapper::Put(string key, string value) {
+   return db->Put(leveldb::WriteOptions(), key, value);
+}
+
+//Test
+int main(){
+    LevelDBWrapper levelDBWrapper;
+    Status s = levelDBWrapper.Put("k1","v1");
+    if (!s.ok()) cerr << s.ToString() << endl;
+}
---
+++ src/util/levelDBWrapper.h
@@ -0,0 +1,36 @@
+#ifndef LEVELDBWRAPPER_H
+#define LEVELDBWRAPPER_H
+
+#include <cassert>
+#include <iostream>
+#include "leveldb/*"
+
+/******************************************************************************
+ * NAMESPACES
+ *****************************************************************************/
+using namespace std;
+using namespace leveldb;
+
+/******************************************************************************
+ * DECLARATION
+ *****************************************************************************/
+
+class LevelDBWrapper {
+    DB* db;
+    Options options;
+    WriteOptions write_options;
+    ReadOptions read_options;
+    
+    public:
+    LevelDBWrapper(){
+        options.create_if_missing = true;
+        Status status = DB::Open(options, "/tmp/testdb", &db);
+        assert(status.ok());
+        write_options.sync = true;
+    }
+   
+   Status Get(string key, string &value);
+   Status Put(string key, string value);
+};
+
+#endif
