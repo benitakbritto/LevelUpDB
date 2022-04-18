61e301b9001b77c6a4f7d20e1aa267b62433fae8
benitakbritto <benita.k.britto@gmail.com>
2022-04-18 00:15:16 +0000

    add term vote volatile

--- src/util/common.h
+++ src/util/common.h
@@ -11,6 +11,7 @@
 #define CRASH_TEST                  0
 #define crash()                     if (CRASH_TEST) { *((char*)0) = 0; }
 #define REPLICATED_LOG_PATH         "/home/benitakbritto/CS739-P4/storage/replicated_log"
+#define TERM_VOTE_PATH              "/home/benitakbritto/CS739-P4/storage/term_vote"
 #define HB_SLEEP_IN_SEC             5
 #define LEADER_STR "LEADER"
 #define FOLLOWER_STR "FOLLOWER"
