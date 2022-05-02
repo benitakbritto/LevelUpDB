b317383ed71cf39fd4ccaa941455609302853e21
benitakbritto <ssharma@node-1.cs739-p4.advosuwmadison-pg0.wisc.cloudlab.us>
2022-05-02 16:40:07 -0500

    add helpers for delete (to test)

--- src/server/resource_alloc_listener.cc
+++ src/server/resource_alloc_listener.cc
@@ -1,4 +1,5 @@
 #include "resource_alloc_listener.h"
+#include <unordered_map>
 
 /******************************************************************************
  * NAMESPACES
@@ -13,6 +14,23 @@
 /******************************************************************************
  * GLOBALS
  *****************************************************************************/
+unordered_map<string, int> serverMap; // kvIp:raftIp, pid
+
+
+// TODO: Error handling
+void AddToMap(string kvIp, string raftIp, int serverPid)
+{
+    string key = kvIp + ":" + raftIp;
+    serverMap[key] = serverPid;
+}
+
+// TODO: Error handling
+int GetPidOfServer(string kvIp, string raftIp)
+{
+    string key = kvIp + ":" + raftIp;
+    return serverMap[key];
+}
+
 
 /******************************************************************************
  * DECLARATION
@@ -22,7 +40,7 @@ grpc::Status ResourceAllocator::AddServer(ServerContext* context, const AddServe
 {
     dbgprintf("[DEBUG] %s: Entering function\n", __func__);
     int forkReturnCode = fork();
-    // fork failed
+    
     if (forkReturnCode < 0)
     {
         dbgprintf("[ERROR] fork failed\n");
@@ -36,22 +54,26 @@ grpc::Status ResourceAllocator::AddServer(ServerContext* context, const AddServe
         args[2] = (char *) request->raft_ip().c_str();
         args[3] = (char *) request->lb_ip().c_str();
         args[4] = (char *) NULL;
+        
+        dbgprintf("[DEBUG] %s: Child pid = %d\n", __func__, getpid());
+        AddToMap(request->kv_ip(), request->raft_ip(), getpid());
+        
         execv("./server", args);
         dbgprintf("[WARN] should not print\n");
     }
-    else
-    {
-        // TODO: Remove
-        dbgprintf("[INFO] parent\n");
-    }
+
     dbgprintf("[DEBUG] %s: Exiting function\n", __func__);
     return grpc::Status::OK;
 }
 
-// TODO
+// TODO Test
 grpc::Status ResourceAllocator::DeleteServer(ServerContext* context, const DeleteServerRequest* request, DeleteServerReply* reply) 
 {
     dbgprintf("[DEBUG] %s: Entering function\n", __func__);
+    int pid = GetPidOfServer(request->kv_ip(), request->raft_ip());
+    dbgprintf("[DEBUG] %s: pid = %d\n", __func__, pid);
+    string command = "kill -9 " + to_string(pid);
+    system(command.c_str());
     dbgprintf("[DEBUG] %s: Exiting function\n", __func__);
     return grpc::Status::OK;
 }
